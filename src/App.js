0d39aae736802d24c844f9451e00300b0ef51986
mhd-sinan-m <shialabconsole@gmail.com>
2024-11-20 22:23:54 +0530

    loop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from "react"
+//TODO: import logo from './logo.svg';
 import './App.css';
+// import Counter from "./counter";
+import Loop from "./loop"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    a: "counter"
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {/* <Counter /> */}
+        <Loop />
+      </div>
+    )
+  }
 }
 
 export default App;
